a8628add9a8131e0b25378fee3f26fccaf0abbbb
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-03-27 04:31:12 +0800

    Update diy-scripts.sh

--- bash/diy-scripts.sh
+++ bash/diy-scripts.sh
@@ -18,17 +18,17 @@ sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.
 sed -i "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" package/base-files/files/etc/banner
 #sed -i '/logins./a\                                          by Jason' package/base-files/files/etc/profile
 # Modify default passwd
-sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
 # ID
 #sed -i "s/DISTRIB_REVISION='R.*.*.[0-9]/& Compiled by Jason/" package/lean/default-settings/files/zzz-default-settings
 # 添加新主题  
-rm -rf ./feeds/luci/themes/luci-theme-argon
-rm -rf ./feeds/haibo/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
+#rm -rf ./feeds/luci/themes/luci-theme-argon
+#rm -rf ./feeds/haibo/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # 主题背景
 
-svn co https://github.com/Joshua-DinG/Build-OpenWRT/trunk/argon/video/default ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/
-rm -rf ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/.svn/
+#svn co https://github.com/Joshua-DinG/Build-OpenWRT/trunk/argon/video/default ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/
+#rm -rf ./feeds/luci/themes/luci-theme-argon-mod/htdocs/luci-static/argon/background/.svn/
 # 临时
 #sed -i s/5.15/6.1/g  target/linux/x86/Makefile
